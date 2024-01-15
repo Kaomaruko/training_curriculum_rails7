724b3e14bbc6e1f840f86e2c5a20135ffbd962f1
Kaomaruko <kaori.mikan1111@gmail.com>
2024-01-15 17:45:36 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,6 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    plan_params
     get_week
     @plan = Plan.new
   end
@@ -16,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
